dbda8804ee85b8650e1de3739c77c58e8a5d49cd
rjames187 <rory.james2021@gmail.com>
2024-01-03 22:27:37 -0600

    create script for running api server container locally

---
+++ servers/gateway/run.sh
@@ -0,0 +1,11 @@
+# use this script to run the container locally with self-signed certs
+
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj "//CN=localhost" -keyout privkey.pem -out fullchain.pem
+
+docker run -d \
+--name gateway \
+-p 443:443 \
+-v $(pwd):/etc/certs:ro \
+-e TLSCERT=/etc/certs/fullchain.pem \
+-e TLSKEY=/etc/certs/privkey.pem \
+rjames187/gateway:1.0
