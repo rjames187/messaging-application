461fb9dfb892f5eab150bb61a7b99ff02d50a33e
rjames187 <rory.james2021@gmail.com>
2024-01-07 14:49:35 -0600

    modify deploy scripts to work with DO

--- servers/gateway/deploy.sh
+++ servers/gateway/deploy.sh
@@ -1,7 +1,7 @@
+# change the ip address in the ssh line as needed
 ./build.sh
 docker push rjames187/gateway:1.0
-ssh -tt ec2-user@ec2-54-87-136-143.compute-1.amazonaws.com << END
-sudo service docker start
+ssh -tt root@142.93.0.40 << END
 docker rm -f gateway
 docker pull rjames187/gateway:1.0
 docker run -d --name gateway -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt:ro -e TLSCERT=\$TLSCERT -e TLSKEY=\$TLSKEY rjames187/gateway:1.0
