37b0e928fd528c5fa94f8ccc83cf72f3789aa786
rjames187 <rory.james2021@gmail.com>
2024-01-07 17:19:33 -0600

    refactor tf config to use 1click docker image for bug-free docker installation

--- infrastructure/gateway.tf
+++ infrastructure/gateway.tf
@@ -1,10 +1,10 @@
-# change the email and domain name in line 30 as needed
+# change the email and domain name in line 25 as needed
 
 resource "digitalocean_droplet" "gateway" {
-  image = "ubuntu-20-04-x64"
+  image = "docker-20-04"
   name = "gateway"
   region = "nyc1"
-  size = "s-1vcpu-512mb-10gb"
+  size = "s-1vcpu-1gb"
   ssh_keys = [
     data.digitalocean_ssh_key.terraform.id
   ]
@@ -21,12 +21,7 @@ resource "digitalocean_droplet" "gateway" {
       "sudo ufw allow 80",
       "sudo ufw allow 443",
       "sudo apt update",
-      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo add-apt-repository -y \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",
-      "apt-cache policy docker-ce",
-      "sudo apt-get install -y docker-ce",
-      "sudo apt-install -y letsencrypt",
+      "sudo apt install -y letsencrypt",
       "sudo letsencrypt certonly --standalone -n --agree-tos --email rory.james2021@gmail.com -d api.rjames.me",
     ]
   }
