2ddf146ab6d75e062b77a638c55d8ef8eea333ac
rjames187 <rory.james2021@gmail.com>
2024-01-07 12:18:18 -0600

    modify tf vm configs to correctly install docker

--- infrastructure/client.tf
+++ infrastructure/client.tf
@@ -16,13 +16,12 @@ resource "digitalocean_droplet" "client" {
   provisioner "remote-exec" {
     inline = [
       "export PATH=$PATH:/usr/bin",
-      # install docker
       "sudo apt update",
-      "sudo apt install apt-transport-https ca-certificates curl software-properties-common",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",
+      "sudo add-apt-repository -y \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",
       "apt-cache policy docker-ce",
-      "sudo apt install docker-ce"
+      "sudo apt-get install -y docker-ce"
     ]
   }
 }
