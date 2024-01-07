dbfad1217dc007ec230c597337c2a4964223d709
rjames187 <rory.james2021@gmail.com>
2024-01-07 16:40:44 -0600

    modify deploy scripts and tf config to install TLS certs

--- infrastructure/gateway.tf
+++ infrastructure/gateway.tf
@@ -1,3 +1,5 @@
+# change the email and domain name in line 30 as needed
+
 resource "digitalocean_droplet" "gateway" {
   image = "ubuntu-20-04-x64"
   name = "gateway"
@@ -16,12 +18,16 @@ resource "digitalocean_droplet" "gateway" {
   provisioner "remote-exec" {
     inline = [
       "export PATH=$PATH:/usr/bin",
+      "sudo ufw allow 80",
+      "sudo ufw allow 443",
       "sudo apt update",
       "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
       "sudo add-apt-repository -y \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",
       "apt-cache policy docker-ce",
-      "sudo apt-get install -y docker-ce"
+      "sudo apt-get install -y docker-ce",
+      "sudo apt-install -y letsencrypt",
+      "sudo letsencrypt certonly --standalone -n --agree-tos --email rory.james2021@gmail.com -d api.rjames.me",
     ]
   }
 }
--- servers/gateway/deploy.sh
+++ servers/gateway/deploy.sh
@@ -1,7 +1,10 @@
-# change the ip address in the ssh line as needed
+# change the ip address in line 4 as needed
+# change domain names in lines 6 and 7 as needed
 ./build.sh
 docker push rjames187/gateway:1.0
 ssh -tt root@142.93.0.40 << END
+export TLSCERT=/etc/letsencrypt/live/api.rjames.me/fullchain.pem
+export TLSKEY=/etc/letsencrypt/live/api.rjames.me/privkey.pem
 docker rm -f gateway
 docker pull rjames187/gateway:1.0
 docker run -d --name gateway -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt:ro -e TLSCERT=\$TLSCERT -e TLSKEY=\$TLSKEY rjames187/gateway:1.0
