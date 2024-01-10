2c90135205261544b3758d6ae655e4329820afc0
rjames187 <rory.james2021@gmail.com>
2024-01-10 16:48:53 -0600

    create func for getting session state

--- servers/gateway/sessions/session.go
+++ servers/gateway/sessions/session.go
@@ -1,5 +1,7 @@
 package sessions
 
+import "errors"
+
 const SESSIONID_LENGTH = 32
 
 func BeginSession(userID int, secret string, store Store) (string, error) {
@@ -14,6 +16,17 @@ func BeginSession(userID int, secret string, store Store) (string, error) {
 	return sessionToken, nil
 }
 
-func GetSessionState(sessionToken string) (int, error) {
-
+func GetSessionState(sessionToken string, secret string, store Store) (int, error) {
+	valid, sessionID, err := validToken(sessionToken, secret, SESSIONID_LENGTH)
+	if err != nil {
+		return 0, err
+	}
+	if !valid {
+		return 0, errors.New("invalid session token")
+	}
+	userID, err := store.Get(sessionID)
+	if err != nil {
+		return 0, err
+	}
+	return userID, nil
 }
