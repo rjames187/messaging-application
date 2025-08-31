26790c1bb369d2ba4883c8d17754ff023ddd5eda
Rory James <rory.james2021@gmail.com>
2025-08-31 16:25:02 -0700

    write tests for specific user handler

--- servers/gateway/handlers/auth_test.go
+++ servers/gateway/handlers/auth_test.go
@@ -137,3 +137,185 @@ func TestNewUserErrors(t *testing.T) {
 		}
 	}
 }
+
+func TestSpecificUser(t *testing.T) {
+	handler := newContext()
+
+	// create new user
+	jsonData, err := json.Marshal(&users.NewUser{
+		Password:  "password343",
+		Email:     "valid_email@example.com",
+		FirstName: "Jon",
+		LastName:  "Doe",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, "/v1/users", bytes.NewReader(jsonData))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+	if rr.Code != http.StatusCreated {
+		t.Fatal(rr.Body.String())
+	}
+
+	authHeader := rr.Header().Get("Authorization")
+
+	// retrieve the user
+	req, err = http.NewRequest(http.MethodGet, "/v1/users/me", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", authHeader)
+
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Expected status %d, got %d", http.StatusOK, status)
+	}
+	if !strings.Contains(rr.Body.String(), `"id":1`) {
+		t.Error("Expected user ID to be returned")
+	}
+	if !strings.Contains(rr.Body.String(), `"firstName":"Jon"`) {
+		t.Error("Expected first name to be returned")
+	}
+	if !strings.Contains(rr.Body.String(), `"lastName":"Doe"`) {
+		t.Error("Expected last name to be returned")
+	}
+
+	// modify user
+	jsonData, err = json.Marshal(&users.Updates{
+		Email:     "new_email@example.com",
+		FirstName: "NewFirstName",
+		LastName:  "NewLastName",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err = http.NewRequest(http.MethodPatch, "/v1/users/1", bytes.NewReader(jsonData))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", authHeader)
+
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Expected status %d, got %d", http.StatusOK, status)
+	}
+	if !strings.Contains(rr.Body.String(), `"firstName":"NewFirstName"`) {
+		t.Error("Expected first name to be returned")
+	}
+	if !strings.Contains(rr.Body.String(), `"lastName":"NewLastName"`) {
+		t.Error("Expected last name to be returned")
+	}
+}
+
+func TestSpecificUserErrors(t *testing.T) {
+	handler := newContext()
+
+	// create new user
+	jsonData, err := json.Marshal(&users.NewUser{
+		Password:  "password343",
+		Email:     "valid_email@example.com",
+		FirstName: "Jon",
+		LastName:  "Doe",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, "/v1/users", bytes.NewReader(jsonData))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+	if rr.Code != http.StatusCreated {
+		t.Fatal(rr.Body.String())
+	}
+
+	authHeader := rr.Header().Get("Authorization")
+
+	// Missing auth header
+	req, err = http.NewRequest(http.MethodGet, "/v1/users/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("Expected status %d, got %d", http.StatusUnauthorized, status)
+	}
+
+	// Wrong method
+	req, err = http.NewRequest(http.MethodPost, "/v1/users/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", authHeader)
+
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusMethodNotAllowed {
+		t.Errorf("Expected status %d, got %d", http.StatusMethodNotAllowed, status)
+	}
+
+	// Non-existent user
+	req, err = http.NewRequest(http.MethodGet, "/v1/users/999", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", authHeader)
+
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("Expected status %d, got %d", http.StatusNotFound, status)
+	}
+
+	// Forbidden user modification
+	req, err = http.NewRequest(http.MethodPatch, "/v1/users/999", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", authHeader)
+	req.Header.Set("Content-Type", "application/json")
+
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusForbidden {
+		t.Errorf("Expected status %d, got %d", http.StatusForbidden, status)
+	}
+
+	// Wrong content type
+	req, err = http.NewRequest(http.MethodPatch, "/v1/users/me", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", authHeader)
+	req.Header.Set("Content-Type", "text/plain")
+
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusUnsupportedMediaType {
+		t.Errorf("Expected status %d, got %d", http.StatusUnsupportedMediaType, status)
+	}
+}
