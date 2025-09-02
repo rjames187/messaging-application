61ab6008a806c9047f1e7d18d8ad7cfc99eaca2f
Rory James <rory.james2021@gmail.com>
2025-09-01 18:16:04 -0700

    fix transport bugs in server

--- servers/gateway/handlers/cors.go
+++ servers/gateway/handlers/cors.go
@@ -9,11 +9,12 @@ type CORSHandler struct {
 const oneDay = "86400"
 
 func (c *CORSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+
 	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 		w.Header().Set("Access-Control-Max-Age", oneDay)
 		w.WriteHeader(http.StatusOK)
 		return
